458d951a9484fdcf04eafb3d0dfc426b5630e449
Julia Borlase <julia@borlase.co.nz>
2022-10-07 11:39:14 +1300

    table functionality

--- frontend/src/components/article-table.tsx
+++ frontend/src/components/article-table.tsx
@@ -1,55 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Article } from "../types";
 
 type Props = {
-    data: Article[];
-  };
+  data: Article[];
+};
 
+const ArticleTable = ({ data }: Props) => {
+  const [sortedTable, setSortedTable] = useState(data);
 
-  const ArticleTable = ({ data }: Props) => {
-    return (
-      
-      <>
-        {data.map((article) => (
-          
-          <>        
-    <tr>
-      <td>{article.title}</td>
-      <td>{article.type}</td>
-      <td>{article.authors.join(", ")}</td>
-      <td>{article.source}</td>
-      <td>{article.year}</td>
-      <td>{article.doi}</td>
-      <td>
-        <button
-          type="button"
-          // onClick={(event) => handleEditClick(event, data)}
-        >
-          Approve
-        </button>
-        <button type="button" 
-        // onClick={() => handleDeleteClick(data.id)}
-        >
-          Reject
-        </button>
-      </td>
-    </tr> 
+  useEffect(() => {
+    setSortedTable(data);
+  }, [data]);
+
+  const sortByDate = () => {
+    setSortedTable(data.sort((a, b) => a.year - b.year))
+  }
+
+  const removeRow = (id: string) =>{
+    setSortedTable(data.filter((article) => article._id !== id))
+  }
+
+  // const approveRow = () =>{
+  //   setSortedTable(data.filter(()))
+  // }
+
+  return (
     <>
-</>
-{/* Sorting the articles by date */}
-    <button type="button" 
-        onClick={() => data.sort((a, b) => (a.year > b.year) ? 1 : -1)} 
-        >
-          Order By Date
-        </button>
-          </>
+      {sortedTable.map((article) => (
 
-        ))}
-      </>
-    );
-  };
+        <>
+          <tr>
+            <td>{article.title}</td>
+            <td>{article.type}</td>
+            <td>{article.authors.join(", ")}</td>
+            <td>{article.source}</td>
+            <td>{article.year}</td>
+            <td>{article.doi}</td>
+            <td>
+              <button
+                type="button"
+              // onClick={() => (approveRow)}
+              >
+                Approve
+              </button>
+              <button type="button"
+              onClick={() => (removeRow(article._id))}
+              >
+                Reject
+              </button>
+            </td>
+          </tr>
+          <>
+
+          </>
+        </>
+
+      ))}
+      <button type="button"
+        onClick={sortByDate}
+      >
+        Order By Date
+      </button>
+    </>
+  );
+};
 
 export default ArticleTable;
-
-
 
